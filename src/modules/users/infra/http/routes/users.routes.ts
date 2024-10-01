88f0465c2f3e2a2f685a5119cea0f2d6a8467a82
ClaytonCassi <cla_yton@hotmail.com>
2024-10-01 12:59:37 -0300

    incluido role no backend

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -25,6 +25,7 @@ usersRouter.post(
       name: Joi.string().required(),
       email: Joi.string().email().required(),
       password: Joi.string().min(6).required(),
+      role: Joi.string().required(),
     },
   }),
   usersController.create,
